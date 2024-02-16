9cc993281a924f91e3fa1d59aa8f5886624f08aa
zachrousseau <zachrousseau0@gmail.com>
2024-02-16 17:37:47 -0500

    Add program description

--- Rousseau.Assignment.04/Rousseau.Assignment.04.Program.03.py
+++ Rousseau.Assignment.04/Rousseau.Assignment.04.Program.03.py
@@ -1,3 +1,41 @@
+# Program #03:
+
+# Design and implement an object-oriented program for a simple game of blackjack that provides for one
+# player and a dealer (the computer).
+
+# Console
+
+# Blackjack
+# DEALER'S SHOW CARD:
+# 9 of Clubs
+# YOUR CARDS:
+# 2 of Hearts
+# Queen of Clubs
+# Hit or stand? (h/s): h
+# YOUR CARDS:
+# 2 of Hearts
+# Queen of Clubs
+# 7 of Clubs
+# Hit or stand? (h/s): s
+# DEALER'S CARDS:
+# 9 of Clubs
+# 7 of Spades
+# Queen of Spades
+# YOUR POINTS: 19
+# DEALER'S POINTS: 26
+# Yay! The dealer busted. You win!
+# Play again? (y/n): n
+# Come back soon!
+# Specifications
+# • Design and implement the classes for the business tier.
+# • Use functions to implement the user interface tier.
+# • Store the code for each tier in its own file.
+# • If necessary, learn the rules of Blackjack by researching it on the web.
+# • Use a standard 52-card deck of playing cards.
+# • The dealer must continue taking cards until the dealer’s hand has at least 17 points.
+# • Don’t implement betting.
+# • Don’t allow a player to “split” a hand or “double down.” (16-1)
+
 def blackjack():
     import cards
 
